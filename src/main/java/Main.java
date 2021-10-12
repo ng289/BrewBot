07f9e2da3c688b331c0c5f45f6d315196bb894bb
ng289 <89607138+ng289@users.noreply.github.com>
2021-10-11 22:49:49 -0500

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Discord Bot Token
+        String token = "ODkxNzU2MjQwNDAzODU3NDQ4.YVC-tw.IH1maEUdsUeLyB30WdTxJVFHyT4";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!hmmm")) {
+                event.getChannel().sendMessage("*cloaked man removes hood* Hello there.");
+            } else if (event.getMessageContent().equalsIgnoreCase("!BeepBlip")) {
+                event.getChannel().sendMessage("Come here, my little friend. Don't be afraid.");
+            } else if (event.getMessageContent().equalsIgnoreCase("!BeepBlipBlip")) {
+                event.getChannel().sendMessage("Oh, don't worry. He'll be all right");
+            } else if (event.getMessageContent().equalsIgnoreCase("!next")) {
+                event.getChannel().sendMessage("*reckless teenager wakes up* Rest easy, son." +
+                                                        "You've had a busy day. You're fortunate to be " +
+                                                        "all in one piece.");
+            }
+        });
+    }
+
+}
